5a52065da59498a9db1c41ae9e71040f2be278ef
Paris <marvel729@gmail.com>
2025-07-31 23:56:03 -0500

    updated resume viewer to use new Google Drive link

--- src/components/resume/ResumeViewer.tsx
+++ src/components/resume/ResumeViewer.tsx
@@ -2,7 +2,7 @@ const ResumeViewer = () => {
   return (
     <div className="w-full h-screen">
       <iframe
-        src="https://drive.google.com/file/d/1HyAEp4ZKY940RT_MqDnZFSxzRzDk36eY/preview"
+        src="https://drive.google.com/file/d/1KTVvDG8L5Nw8KQVYKLOvSc96A9uoaUyv/preview"
         width="100%"
         height="100%"
         title="Resume PDF"
