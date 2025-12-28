8e1a77f808e6ec3f51d7c42e8a578e5cf3b4a5bc
Paris Cabrera <109258198+PVCabrera@users.noreply.github.com>
2025-12-28 14:24:52 -0600

    Update ResumeViewer iframe source URL

--- src/components/resume/ResumeViewer.tsx
+++ src/components/resume/ResumeViewer.tsx
@@ -2,7 +2,7 @@ const ResumeViewer = () => {
   return (
     <div className="w-full h-screen">
       <iframe
-        src="https://drive.google.com/file/d/1KTVvDG8L5Nw8KQVYKLOvSc96A9uoaUyv/preview"
+        src="https://drive.google.com/file/d/1wcJkww7kFv2TPrgUiPELL8y3eCVjIOll/view"
         width="100%"
         height="100%"
         title="Resume PDF"
